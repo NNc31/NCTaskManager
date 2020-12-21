0ee9c1a5f58e03c768bf41c19f839eb055a924fd
Nefodov <nefodov.nazar@outlook.com>
2020-12-21 19:32:51 +0200

    Practice 7. Added static methods incoming() and calendar()

--- src/main/java/ua/edu/sumdu/j2se/nefodov/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/nefodov/tasks/AbstractTaskList.java
@@ -15,20 +15,6 @@ public abstract class AbstractTaskList implements Iterable, Cloneable {
     public abstract Stream<Task> getStream();
     public abstract ListTypes.types getType();
 
-    public final AbstractTaskList incoming(int from, int to) throws IllegalArgumentException {
-        if (from < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        AbstractTaskList inTime = TaskListFactory.createTaskList(getType());
-
-        Stream<Task> stream = getStream();
-        stream.forEach(task -> {
-            if (task.nextTimeAfter(from) <= to && task.nextTimeAfter(from) != -1) inTime.add(task);
-        });
-        return inTime;
-    }
-
     public int size(){
         return size;
     }
